368cec18a12e8e9f089d1633e3256f0313e3fe52
jaGdish <jagdish.shinde@functionup.org>
2022-12-23 15:30:54 +0530

    add : linux commands

---
+++ command.js
@@ -0,0 +1,9 @@
+// 1) pwd -> give present working directly / current location
+// 2) whoami -> print user name
+// 3) touch fileName -> creates a file
+// 4) sudo nano fileName -> open file to edit on terminal itself
+// 5) sudo geditfileName -> open file to edit in respective editor
+// 6) rm fileName -> removed file
+// 7) mkdir folderName -> create folder
+// 8) rmdir folderName -> removed folder (empty)
+// 9) rm -r folderName -> removed folder (filled)
